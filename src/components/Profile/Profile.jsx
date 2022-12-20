a533b75a379648806517f110e230a1b16fd7f455
Olexii-US <alyosha.us@gmail.com>
2022-12-20 18:19:59 +0200

    3 task done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,47 @@
-// import user from "/user.json";
+import PropTypes from "prop-types";
+import css from "./Profile.module.css";
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className="profile">
-  <div className="description">
+    <div className={css.profile}>
+  <div className={css.description}>
     <img
       src={avatar}
-      alt="User avatar"
-      className="avatar"
+      alt={`${username} avatar`}
+      className={css.avatar}
     />
-    <p className="name">{username}</p>
-    <p className="tag">{tag}</p>
-        <p className="location">{location}</p>
+    <p className={css.name}>{username}</p>
+    <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
   </div>
 
-  <ul className="stats">
+  <ul className={css.stats}>
     <li>
-      <span className="label">{Object.keys(stats)[0]}</span>
-      <span className="quantity">{stats.followers}</span>
+      <span className={css.label}>{Object.keys(stats)[0]}</span>
+      <span className={css.quantity}>{stats.followers}</span>
     </li>
     <li>
-      <span className="label">{Object.keys(stats)[1]}</span>
-      <span className="quantity">{stats.views}</span>
+      <span className={css.label}>{Object.keys(stats)[1]}</span>
+      <span className={css.quantity}>{stats.views}</span>
     </li>
     <li>
-      <span className="label">{Object.keys(stats)[2]}</span>
-      <span className="quantity">{stats.likes}</span>
+      <span className={css.label}>{Object.keys(stats)[2]}</span>
+      <span className={css.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>
   )
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  })
 }
 
