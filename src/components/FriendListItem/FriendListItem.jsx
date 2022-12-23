b41880f22332f19a0e86033a7961241a23b25091
Olexii-US <alyosha.us@gmail.com>
2022-12-23 13:59:24 +0200

    HW fixed

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,29 @@
-import PropTypes from "prop-types";
-import css from "./FriendListItem.module.css";
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
-    return (
-<li className={css.item}>
-     <span className={isOnline ? `${css.status} ${css.statusOnline}` : `${css.status} ${css.statusOffline}`} ></span>
-    <img className={css.avatar} src={avatar} alt={`${name} avatar`} width="48" />
-    <p className={css.name}>{ name }</p>
-</li>
-    )
-}
+  return (
+    <li className={css.item}>
+      <span
+        className={
+          isOnline
+            ? `${css.status} ${css.statusOnline}`
+            : `${css.status} ${css.statusOffline}`
+        }
+      ></span>
+      <img
+        className={css.avatar}
+        src={avatar}
+        alt={`${name} avatar`}
+        width="48"
+      />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
 
 FriendListItem.propTypes = {
-    avatar: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-}
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,37 +1,39 @@
-import PropTypes from "prop-types";
-import css from "./Profile.module.css";
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
-export const Profile = ({ username, tag, location, avatar, stats }) => {
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
   return (
     <div className={css.profile}>
-  <div className={css.description}>
-    <img
-      src={avatar}
-      alt={`${username} avatar`}
-      className={css.avatar}
-    />
-    <p className={css.name}>{username}</p>
-    <p className={css.tag}>@{tag}</p>
+      <div className={css.description}>
+        <img src={avatar} alt={`${username} avatar`} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
-  </div>
+      </div>
 
-  <ul className={css.stats}>
-    <li>
-      <span className={css.label}>{Object.keys(stats)[0]}</span>
-      <span className={css.quantity}>{stats.followers}</span>
-    </li>
-    <li>
-      <span className={css.label}>{Object.keys(stats)[1]}</span>
-      <span className={css.quantity}>{stats.views}</span>
-    </li>
-    <li>
-      <span className={css.label}>{Object.keys(stats)[2]}</span>
-      <span className={css.quantity}>{stats.likes}</span>
-    </li>
-  </ul>
-</div>
-  )
-}
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
@@ -39,9 +41,8 @@ Profile.propTypes = {
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
   stats: PropTypes.shape({
-    followers: PropTypes.number,
-    views: PropTypes.number,
-    likes: PropTypes.number,
-  }).isRequired
-}
-
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
